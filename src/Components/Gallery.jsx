ef6dd4fba0ec39cf5b7739a0e14cf7a0d3498f3f
dohung95 <dohungjp1995@gmail.com>
2024-10-11 00:31:33 +0700

    update

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,8 +1,35 @@
 import '../Css/Van/Gallery.css'
+import Data from '../Data/Van/Gallery.json'
 const Gallery = () => {
+    const path = process.env.PUBLIC_URL
     return (
         <>
-
+            <div className='gallery'>
+                {Data.map((img) => (
+                    <div className='row' key={img.id}>
+                        <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
+                            <div align='center' style={{paddingBottom:"2%"}}>
+                                <img src={path + img.hinh1} />
+                            </div>
+                        </div>
+                        <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
+                            <div align='center' style={{paddingBottom:"2%"}}>
+                                <img src={path + img.hinh2} />
+                            </div>
+                        </div>
+                        <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
+                            <div align='center' style={{paddingBottom:"2%"}}>
+                                <img src={path + img.hinh3} />
+                            </div>
+                        </div>
+                        <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
+                            <div align='center' style={{paddingBottom:"2%"}}>
+                                <img src={path + img.hinh4} />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
