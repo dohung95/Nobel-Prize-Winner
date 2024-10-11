98fdd46e63467da694bd5a06f6e74b54606a7114
dohung95 <dohungjp1995@gmail.com>
2024-10-11 12:40:17 +0700

    update

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -8,23 +8,27 @@ const Gallery = () => {
                 {Data.map((img) => (
                     <div className='row' key={img.id}>
                         <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
-                            <div align='center' style={{paddingBottom:"2%"}}>
+                            <div align='center' className='borderImg'>
                                 <img src={path + img.hinh1} />
+                               <div><i>{img.cap1}</i></div>
                             </div>
                         </div>
                         <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
-                            <div align='center' style={{paddingBottom:"2%"}}>
+                            <div align='center' className='borderImg'>
                                 <img src={path + img.hinh2} />
+                                <div><i>{img.cap2}</i></div>
                             </div>
                         </div>
                         <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
-                            <div align='center' style={{paddingBottom:"2%"}}>
+                            <div align='center' className='borderImg'>
                                 <img src={path + img.hinh3} />
+                                <div><i>{img.cap3}</i></div>
                             </div>
                         </div>
                         <div className='col-md-3 col-sm-6' style={{paddingBottom:"2%"}}>
-                            <div align='center' style={{paddingBottom:"2%"}}>
+                            <div align='center' className='borderImg'>
                                 <img src={path + img.hinh4} />
+                                <div><i>{img.cap4}</i></div>
                             </div>
                         </div>
                     </div>
